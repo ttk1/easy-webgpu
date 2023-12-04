cfec5af2c9936037a2e2373dcb551fe3cb0369da
tama <tama@ttk1.net>
2023-12-04 22:02:05 +0900

    幅が 64 の倍数以外のサイズのテクスチャに対応

--- src/util.ts
+++ src/util.ts
@@ -1,25 +1,20 @@
-export function fetchImage(src: string): Promise<HTMLImageElement> {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = (e) => reject(e);
-    img.src = src;
-  });
-}
-
 export function fetchImageData(src: string): Promise<ImageData> {
   return new Promise((resolve, reject) => {
     const img = new Image();
     img.onload = () => {
       const cvs = document.createElement('canvas');
-      cvs.width = img.width;
+      // 後でテクスチャを読みだす際に一行のバイト数が256の倍数になっている必要があるため
+      // ここで画像データの幅を64の倍数にリサイズするしておく
+      // ref: https://www.w3.org/TR/webgpu/#dom-gpuimagedatalayout-bytesperrow
+      const width = Math.ceil(img.width / 64) * 64;
+      cvs.width = width;
       cvs.height = img.height;
       const ctx = cvs.getContext('2d');
       if (ctx == null) {
         throw new Error('画像の読み込みに失敗しました');
       }
-      ctx.drawImage(img, 0, 0);
-      resolve(ctx.getImageData(0, 0, img.width, img.height));
+      ctx.drawImage(img, 0, 0, width, img.height);
+      resolve(ctx.getImageData(0, 0, width, img.height));
     };
     img.onerror = (e) => reject(e);
     img.src = src;
